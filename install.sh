051cbd042d8ee7ac6dc1a1b6a3066b295d1ba3ef
Mr.programmer2938 <78996423+mrprogrammer2938@users.noreply.github.com>
2021-09-14 22:01:31 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,39 @@
 #       ┃┏━┫┏┓┃┗┛┃┃┏━┓┃┏┓┃┏━┫┗┛┫┃━┫┏┫━━┫
 #       ┃┗━┫┏┓┃┃┃┃┃┃╋┃┃┏┓┃┗━┫┏┓┫┃━┫┃┣━━┃
 #       ┗━━┻┛┗┻┻┻┛┗┛╋┗┻┛┗┻━━┻┛┗┻━━┻┛┗━━┛
-#            version 2.4.0
-#            (cam Hackers)
-clear
-echo "installing... "
-echo "──────────╔╗─╔╗─────╔╗"
-echo "──────────║║─║║─────║║"
-echo "╔══╦══╦╗╔╗║╚═╝╠══╦══╣║╔╦══╦═╦══╗"
-echo "║╔═╣╔╗║╚╝║║╔═╗║╔╗║╔═╣╚╝╣║═╣╔╣══╣"
-echo "║╚═╣╔╗║║║║║║─║║╔╗║╚═╣╔╗╣║═╣║╠══║"
-echo "╚══╩╝╚╩╩╩╝╚╝─╚╩╝╚╩══╩╝╚╩══╩╝╚══╝"
-sudo apt install python3
-sudo apt install python
-sudo apt install firefox
-chmod +x camhackers.py
-echo "finish!"
+#            version 2.0
+#            (Cam Hackers)
+if [[ "$(id -u)" -ne 0 ]];
+then
+  echo "
+  Please, Run This Programm as Root!"
+  exit 1
+fi
+function main() {
+    printf '\033]2;Cam-Hackers-Installing\a'
+    clear
+    echo "
+Installing... "
+    sleep 2
+    chmod +x camhackers.py
+    echo "──────────╔╗─╔╗─────╔╗"
+    echo "──────────║║─║║─────║║"
+    echo "╔══╦══╦╗╔╗║╚═╝╠══╦══╣║╔╦══╦═╦══╗"
+    echo "║╔═╣╔╗║╚╝║║╔═╗║╔╗║╔═╣╚╝╣║═╣╔╣══╣"
+    echo "║╚═╣╔╗║║║║║║─║║╔╗║╚═╣╔╗╣║═╣║╠══║"
+    echo "╚══╩╝╚╩╩╩╝╚╝─╚╩╝╚╩══╩╝╚╩══╩╝╚══╝"
+    echo "v2.0"
+    apt install python3
+    apt install python
+    apt install firefox
+    chmod +x camhackers.py
+    echo "
+Finish!
+
+Usage:
+     python3 camhackers.py 
+"
+    exit 1
+
+}
+main
